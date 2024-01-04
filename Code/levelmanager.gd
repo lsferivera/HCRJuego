1d7ca336487722784c11d76e6b3e9817d16abd90
raymich <diegoparrales2005@gmail.com>
2024-01-04 18:37:55 -0500

    PAUSE

--- Code/levelmanager.gd
+++ Code/levelmanager.gd
@@ -1,4 +1,6 @@
 extends Node2D
+@onready var pause_menu = $PauseMenu
+var paused = false
 
 var coins_collected = 0
 
@@ -9,3 +11,15 @@ func add_coins(amount):
 func update_fuel_UI(value):
 	$UI/Fuel/ProgressBar.value = value
 	var stylebox = $UI/Fuel/ProgressBar.get('custom_style/fg')
+
+func pause(state):
+	if state:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		pause_menu.hide()
+		get_tree().paused = false
+
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().paused = true
+		pause_menu.show()
+		
