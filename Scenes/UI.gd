00569b0caa74663c105ef8677f331eb1cde7f0d6
lsferivera <lfriveramora@hotmail.com>
2024-01-04 22:02:58 -0500

    jiji
    botones

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -12,15 +12,22 @@ func _process(delta):
 func _on_button_pressed():
 	get_tree().paused = true
 	menu.visible = get_tree().paused
+	$Botones/Izquierda.visible = false
+	$Botones/Derecha.visible = false
 
 
 func _on_resume_pressed():
 	get_tree().paused = false
 	menu.visible = get_tree().paused
+	$Botones/Izquierda.visible = true
+	$Botones/Derecha.visible = true
 
 
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
-	
+	$Botones/Izquierda.visible = true
+	$Botones/Derecha.visible = true
 
+func _on_dere_pressed():
+	get_tree().Input.is_action_pressed('ui_right')
